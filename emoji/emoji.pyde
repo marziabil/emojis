6cd7ed474f9a21ed158bef65e40316e3774f8b0f
Marzia Bilwani <94219257+marziabil@users.noreply.github.com>
2022-07-05 15:46:33 +0100

    Add files via upload

--- emoji/emoji.pyde
+++ emoji/emoji.pyde
@@ -1,27 +1,21 @@
 from face import Face
-from left_eye import LeftEye
-from right_eye import RightEye
+from eyes import Eyes
 from mouth import Mouth
-from left_eyebrow import lEyeBrow
-from right_eyebrow import rEyeBrow
+from eyebrows import Eyebrows
+
 
 def setup(): 
     size(200, 200)
-    #background(0)
-    global face1, eyeL, eyeR, mouth1, ebL, ebR
+    global face1, eyes, mouth1, eyeBrows
     face1 = Face()
-    eyeL = LeftEye('oval') #eye type - enter frown, oval or v shaped
-    eyeR = RightEye('oval') # eye type - frown, oval or v shaped
+    eyes = Eyes('x') #eye type - enter frown, oval or v shaped
     mouth1 = Mouth( 'frown') # mouth type, frown, open or zig-zag
-    ebL = lEyeBrow('none') #left eyebrow - none, horizontal, high or slightly furrowed
-    ebR = rEyeBrow('none') #right eyebrow - none, horizontal, high or slightly furrowed
+    eyeBrows = Eyebrows('horizontal') #none, horizontal, high or slightly furrowed
     
 def draw():
     background(255)
     face1.display()
-    eyeL.display()
-    eyeR.display()
+    eyes.display()
     mouth1.display()
-    ebL.display()
-    ebR.display()
+    eyeBrows.display()
     
--- emoji/eyebrows.py
+++ emoji/eyebrows.py
@@ -1,28 +1,31 @@
-'''
-class rEyeBrow(object):
+class Eyebrows(object):
     
     #constructor
     def __init__(self, ebType):
         self.ebType = ebType #type of eyebrow
         
     def display(self):
-        #horizontal arc
         if self.ebType == 'none':
             noStroke()
             noFill()
+        #horizontal arc
         elif self.ebType == 'horizontal':
             strokeWeight(5)
             noFill()
             stroke(204, 153, 0)
-            arc(125, 65, 37.5, 12.5, PI, TWO_PI)
+            arc(75, 65, 37.5, 12.5, PI, TWO_PI) #left eyebrow
+            arc(125, 65, 37.5, 12.5, PI, TWO_PI) #right eyebrow
         elif self.ebType == 'high': #highly furrowed
-            stroke(204, 153, 0)
-            noFill()
+            stroke(204, 153, 0) #color of eyebrow
             strokeWeight(6) 
-            line(115, 40, 140, 65)
+            line(60, 65, 85, 40) #left eyebrow
+            line(115, 40, 140, 65) #right eyebrow
         else: #slightly furrowed
             stroke(204, 153, 0)
             strokeWeight(6)
             noFill()
-            arc(150, 50, 80, 30, HALF_PI, PI)
-            '''
+            arc(50, 50, 80, 30, 0, HALF_PI) #left eyebrow
+            arc(150, 50, 80, 30, HALF_PI, PI) #right eyebrow
+        
+        
+    
