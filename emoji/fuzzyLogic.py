841f2e8ebdf199046ddcc43ba1e6a8daf2975438
Marzia Bilwani <marziabilwani@gmail.com>
2022-07-14 15:10:30 +0100

    fuzzy logic

--- emoji/fuzzyLogic.py
+++ emoji/fuzzyLogic.py
@@ -1,29 +1,43 @@
 from ast import Is
 import simpful as sf
-from eyebrows import Eyebrows
+#from eyebrows import Eyebrows
 
 FS = sf.FuzzySystem()
 
-#define a linguistic variable
-S_1 = sf.FuzzySet( points=[[0,0.125], [12.5, 0.125], [12.5,0]] , term="no_eyebrows" )
-S_2 = sf.FuzzySet( points=[ [0,0.05],[27.5,0.15], [27.5,0]], term="horizontal")
-S_3 = sf.FuzzySet( points = [ [0,0.15], [35,0.4], [40,0]], term="slightly_furrowed")
-S_4 = sf.FuzzySet( points = [ [0,0.395], [39.5,0]], term="highly_furrowed" )
-FS.add_linguistic_variable("EB", sf.LinguisticVariable( [S_1, S_2, S_3, S_4]))
+#define linguistic variable - arousal
+S_1 = sf.FuzzySet( points=[[0,1], [ 1,1], [2,1]] , term="very_low" )
+S_2 = sf.FuzzySet( points=[ [1,0],[2,1], [3.5,1], [5,0]], term="low")
+S_3 = sf.FuzzySet( points = [ [3,0], [4,1], [5.5, 1], [7,0]], term="medium")
+S_4 = sf.FuzzySet( points = [ [5,0], [5.5,1], [8.5,1], [9,0]], term="high" )
+S_5 = sf.FuzzySet( points= [[8,0], [9,1], [10,1]], term="very_high")
+FS.add_linguistic_variable("Arousal", sf.LinguisticVariable( [S_1, S_2, S_3, S_4, S_5]))
 
-# Define consequents
-FS.set_crisp_output_value("NO_EYEBROWS", 0)
-FS.set_crisp_output_value("HORIZONTAL", 25)
-FS.set_crisp_output_value("LOW", 50)
-FS.set_crisp_output_value("HIGH", 75)
+#define linguistic variable - valence
+F_1 = sf.FuzzySet( points=[[0,1], [ 1,1], [2,1]] , term="very_low" )
+F_2 = sf.FuzzySet( points=[ [1,0],[2,1], [3.5,1], [5,0]], term="low")
+F_3 = sf.FuzzySet( points = [ [3,0], [4,1], [5.5, 1], [7,0]], term="medium")
+F_4 = sf.FuzzySet( points = [ [5,0], [5.5,1], [8.5,1], [9,0]], term="high" )
+F_5 = sf.FuzzySet(points= [[8,0], [9,1], [10,1]], term="very_high")
+FS.add_linguistic_variable("Valence", sf.LinguisticVariable( [F_1, F_2, F_3, F_4, F_5]))
+
+# Define output crisp values
+FS.set_crisp_output_value("VERY LOW", -0.6)
+FS.set_crisp_output_value("LOW", -0.2)
+FS.set_crisp_output_value("MEDIUM", 0.2)
+FS.set_crisp_output_value("HIGH", 0.6)
+FS.set_crisp_output_value("VERY HIGH", 1)
 
 # Define fuzzy rules.
-RULE1 = "IF(EB IS no_eyebrows) THEN (Eyebrows IS NO_EYEBROWS)"
-RULE2 = "IF(EB IS horizontal) THEN (Eyebrows IS HORIZONTAL)"
-RULE3 = "IF (EB IS slightly_furrowed) THEN (Eyebrows IS LOW)"
-RULE4 = "IF (EB IS highly_furrowed) THEN (Eyebrows IS HIGH) "
-FS.add_rules([RULE1, RULE2, RULE3, RULE4])
+#Mouth
+#very low mouth is zigzag, low is frowning, 
+RULE1 = "IF(Valence IS low) AND (Arousal IS low) THEN (Mouth IS LOW)"
+RULE2 = "IF(Valence IS very_low) THEN (Mouth IS VERY LOW)"
+RULE3 = "IF (Valence IS low) AND (Arousal IS very_low) THEN (Mouth IS VERY LOW)" #open mouth. how do i indicate this?
+FS.add_rules([RULE1, RULE2, RULE3])
 
-# Set antecedents values, perform Sugeno inference and print output values.
-FS.set_variable("OXI", .51)
-print (FS.Sugeno_inference(['POWER']))
+# Set antecedents values
+FS.set_variable("Valence", 4)
+FS.set_variable("Arousal", 8)
+
+# Perform Mamdani inference and print output
+print(FS.Mamdani_inference(["Tip"]))
