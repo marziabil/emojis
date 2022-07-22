45763383a97f785c43ed6725a0ee8c3d96d610c3
Marzia Bilwani <marziabilwani@gmail.com>
2022-07-22 11:03:33 +0100

    face color updates

--- emoji/face.py
+++ emoji/face.py
@@ -5,30 +5,16 @@ class Face(object):
         self.xValence = xValence #x coordinate of emotion - valence
         self.yArousal = yArousal #y coordinate of emotion - arousal
 
-    # def faceColor(self):
-    #     strokeWeight(5)
-    #     stroke(0)
-    #     ellipse(width * 0.5, height * 0.5, 150, 150)
+    def drawFace(self):
+        strokeWeight(5)
+        stroke(0)
+        ellipse(width * 0.5, height * 0.5, 150, 150)
 
-    # def display(self):
-    #     if self.xValence < 0 and self.xValence >= -0.4 and self.yArousal >= 0.4 and self.yArousal <= 0.75: 
-    #         def faceColor(self): 
-    #             fill(255, 204, 0) #red color face
-    #     else: 
-    #         def faceColor(self): 
-    #             fill(255, 80, 80)    
-
-    
     def display(self):
-        if self.xValence < 0 and self.xValence >= -0.4 and self.yArousal >= 0.4 and self.yArousal <= 0.75:
-        #red color face
-            strokeWeight(5)
-            stroke(0)
-            fill(255, 204, 0)
-            ellipse(width * 0.5, height * 0.5, 150, 150)
+        if self.xValence < 0 and self.xValence >= -0.4 and self.yArousal >= 0.4 and self.yArousal <= 0.75: 
+            fill(255, 204, 0) #red color face
+            self.drawFace()
         else: 
-        #yellow color face
-            strokeWeight(5)
-            stroke(0)
-            fill(255, 80, 80)
-            ellipse(width * 0.5, height * 0.5, 150, 150)
+            fill(255, 80, 80) #yellow face
+            self.drawFace()
+           
