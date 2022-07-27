761021230750ccaf085e4a7969d73aa82e71a1fb
Marzia Bilwani <marziabilwani@gmail.com>
2022-07-27 15:55:42 +0100

    py5 test

---
+++ face.py
@@ -0,0 +1,23 @@
+# from py5 import Sketch
+
+# class Face(Sketch):
+
+#     def __init__(self, faceColour):
+#         # self.xValence = xValence #x coordinate of emotion - valence
+#         # self.yArousal = yArousal #y coordinate of emotion - arousal
+#         self.faceColour = faceColour
+
+#     def makeFace(self):
+#         self.stroke_weight(5)
+#         self.stroke(0)
+#         self.ellipse(self.width * 0.5, self.height * 0.5, 150, 150)
+
+#     def display(self):
+#         if self.faceColour == "yellow":
+#             self.makeFace()
+#             self.fill(255, 204, 0) #yellow face
+#         else: 
+#             self.fill(255, 80, 80) #red color face
+#             self.makeFace()
+           
+        
---
+++ main.py
@@ -0,0 +1,67 @@
+'''
+The emoji class produces a set of emoji with different features i.e. eyes, mouth and eyebrows
+These can be tweaked by entering the x and y coordinates of the emotion from the circumplex model. 
+'''
+# from py5 import Sketch
+import py5
+from face import Face
+from eyes import Eyes
+from mouth import Mouth
+from eyebrows import Eyebrows
+# from fuzzyLogic import mouthForEmoji
+# from fuzzyLogic import mouthForEmoji, mouthType
+# from simpful import FuzzySystem, FuzzySet, Trapezoidal_MF, LinguisticVariable
+
+#module mode
+
+faceColour = "yellow"
+eyeType = "wideOpenEyes"
+mouthType = "happy"
+# mouthType = mouthForEmoji()
+ebType = "no_eyebrows"
+
+def setup():
+    py5.size(200,200)
+    py5.background(255)
+    
+    global face, eyes, mouth, eyeBrows
+    face = Face(faceColour)
+    eyes = Eyes(eyeType) 
+    mouth = Mouth(mouthType) 
+    eyeBrows = Eyebrows(ebType) 
+
+def draw():
+    face.display()
+    eyes.display()
+    mouth.display()
+    eyeBrows.display()
+
+py5.run_sketch()
+
+
+
+
+#CLASS MODE
+# class Emoji(Sketch):
+
+#     def settings(self):
+#         self.size(200,200)
+#         self.background(255)
+    
+#     def setup(): 
+#         global face, eyes, mouth, eyeBrows
+#         face = Face(faceColour)
+#         eyes = Eyes(eyeType) 
+#         mouth = Mouth(mouthType) 
+#         eyeBrows = Eyebrows(ebType)  
+    
+#     def draw():
+#         face.display()
+#         eyes.display()
+#         mouth.display()
+#         eyeBrows.display()
+#     # mouthType()
+
+# emoji = Emoji()
+# emoji.run_sketch()
+
